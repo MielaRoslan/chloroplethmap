a48cc23fd45964ceb5b77ab3705d661636c2f564
Syamiela Roslan <nursyamiela194@gmail.com>
2022-02-17 15:49:51 +0800

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log(d3)
+console.log(topojson)
+
+let eduURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+
+let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+
+let eduData
+let countyData
+
+let canvas = d3.select(#canvas)
+
+let drawMap = () => {
+    
+}
